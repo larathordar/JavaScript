5406ffe0e88fbdab7435eec88cb9a8d9152eee6b
Lara Thordardottir <lara-thordar@hotmail.com>
2018-02-01 16:45:46 +0100

    JavaScript exercises

--- java.js
+++ java.js
@@ -4,14 +4,13 @@
 // Create an array and add three numbers to it.
 arr = [1 ,2 ,3];
 // Use your array to return the second number.
-alert(arr[1]);
+console.log(arr[1]);
 // What data type is 123/12? "Things in quotes!"? undefined?
-number = typeof(123/12);
+typeof(123/12); //is a number
 // number
-string = typeof('hello');
+typeof("Things in quotes!"); //is a string
 // String
-unDefined = typeof(undefined);
-// undefined var i=0; i<=n; i++
+typeof(undefined); //is undefined
 
 // Write an if statement that returns true.
 var a = 2;
@@ -31,4 +30,76 @@ emptyArray = [];
 
 for (var i=0; i<myArray.length; i++) {
   emptyArray.push(myArray[i]);
+}
+console.log(emptyArray)
+
+
+//Javascript Exercises #2 !!
+
+//Write a function that returns your first name. Call it.
+
+function me(){
+  return "Lara"
+}
+
+me() //Lara
+
+
+//Write a new function that takes your name as an input. The function should return your first name, plus your last name, as one string. (Hint: strings can be combined with a +)
+function name(first, last){
+  console.log(`My name is ${first} ${last}`)
+}
+
+name('Lara', 'Thordardottir')
+
+//Javascript Exercise #3
+//What is this? Does it have an equivalent in Ruby?
+
+//this is determined by how a function is called. It is similar to self in Ruby.
+
+//What is this? Does it have an equivalent in Ruby?
+
+
+//Convert the above Person class to an ES2015 version using the new class syntax
+
+class Person
+function(first, last) {
+
+    this.first = first;
+    // public variables -- reference current object
+    this.last = last;
+
+    var privateFn = function(first, last){
+    // private function
+    }
+
+    this.setName = function(first, last){
+        // public function
+        this.first = first;
+        this.last = last;
+    }
+
+}
+
+fullName = () => {
+    // extend prototype
+    return this.first + ' ' + this.last;
+    // even at runtime!
+}
+
+var bob = new Person("Thomas", "Ochman");
+    // "new" creates an object
+bob.fullName();
+    // "Thomas Ochman"
+
+    const privateFn = (first, last) => {
+      // private function
+    };
+
+    this.setName = (first, last) => {
+        // public function
+        this.first = first;
+        this.last = last;
+    };
+  }
 }
